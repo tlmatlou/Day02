d3e2a13bebe5ad44fa75b20782e4116d6c676040
Tlou Matlou <tlmatlou@c4r21s1.wethinkcode.co.za>
2020-07-10 08:51:04 +0200

    ex03 final

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,18 +6,33 @@
 /*   By: tlmatlou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 16:45:34 by tlmatlou          #+#    #+#             */
-/*   Updated: 2020/07/08 17:10:43 by tlmatlou         ###   ########.fr       */
+/*   Updated: 2020/07/10 08:49:26 by tlmatlou         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+	/* ************************************************************************** */
 #include<unistd.h>
-void ft_is_negative(int i)
-{
-	
-	if(i>0)
+		
+	void ft_putchar(char c)
 	{
-		write(1,"P",1);
+		write(1,&c,1);
+
 	}
-	else 
-		write(1,"N",1);
+	void ft_is_negative(int i)
+	{
+		char p = 'P';
+		char n = 'N';
+		
+		if(i>0)
+		{
+			//write(1,"P",1);
+			ft_putchar(p);
+		}
+		else 
+			//write(1,"N",1);
+		ft_putchar(n);
 }
 
+int main(void)
+{
+	ft_is_negative(4);
+	return 0;
+}
